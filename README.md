544220b2c380de06723ec637156f0201e7ff454c
Andrea Macchieraldo <a.macchieraldo@gmail.com>
2023-05-26 16:20:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# drone-matternotify
+
+![Docker Pulls](https://img.shields.io/docker/pulls/macchie/drone-matternotify?style=for-the-badge) ![Docker Image Size (latest by date)](https://img.shields.io/docker/image-size/macchie/drone-matternotify?style=for-the-badge)
+
+
+`drone-matternotify` is a Drone Plugin used to send notifications to `Mattermost` via `Incoming Webhooks`.
+
+### Usage
+
+Add a new Step in your `.drone.yml` file:
+
+```yml
+[...]
+
+- name: MatterNotify
+  image: macchie/drone-matternotify
+  settings:
+    webhook: "https://your.mattermost.hosted/hooks/123456567657567567"
+    channel: "#m-testing"
+    username: "MatterNotify Custom Username"
+    icon_url: "https://i.postimg.cc/VkyND0vs/bot.png"
+    title: "A new hope on {{DRONE_REPO_OWNER}}/{{DRONE_REPO_NAME}}, version is now {{DRONE_TAG}}!"
+    title_link: "https://github.com/{{DRONE_REPO_OWNER}}/{{DRONE_REPO_NAME}}/commit/{{DRONE_COMMIT}}"
+    text: ":rocket: Released by [{{DRONE_COMMIT_AUTHOR_NAME}}](mailto:{{DRONE_COMMIT_AUTHOR_EMAIL}})."
+    mailto: "team-manager@example.com"
+    mailto_subject: "Version is now {{DRONE_TAG}}!"
+    mailto_label: "Share the Secret!"
+    mailto_cc:
+      - "quality@example.com"
+      - "marketing@example.com"
+      - "{{DRONE_COMMIT_AUTHOR_EMAIL}}"
+
+[...]
+```
+
+### Parameters
+
+- `webhook`: **mandatory**, your Mattermost Incoming WebHook URL
+- `channel`: **mandatory**, your Mattermost Channel Name
+- `username`: **optional**, depending on your Mattermost configuration this could either be forced on the WebHook definition or the default for your Mattermost setup
+- `icon_url`: **optional**, depending on your Mattermost configuration this could either be forced on the WebHook definition or the default for your Mattermost setup
+- `title`: **optional**, defaults to `New release of {{DRONE_REPO}}@{{DRONE_TAG}}!`
+- `title_link`: **optional**
+- `text`: **optional**, additional text to append to your Notification
+- `mailto`: **optional**, if specified automatically prepends a link to the message to send an email to the specified address
+- `mailto_cc`: **optional**, add cc recipients to your `mailto`
+- `mailto_subject`: **optional**, customize your email subject, defaults to `title`
+- `mailto_label`: **optional**, customize link text in message, defaults to `Send notification email`
+
+##### About Variables Substitution
+
+You are free to embed every environment variable in the following parameters:
+
+- `title`
+- `title_link`
+- `text`
+- `mailto_cc`
+- `mailto_label`
+- `mailto_subject`
+
+Variables may be added inside text using the syntax `{{ENV_VARIABLE_NAME}}`, for example: `{{DRONE_REPO}}`.
+
+Check Drone Docs for all the available Environment variables [here](https://docs.drone.io/pipeline/environment/reference/).
---
+++ matternotify.js
@@ -0,0 +1,112 @@
+#!/usr/bin/env node
+
+const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+class MatterNotify {
+
+  static async execute() {
+    if (process.env.PLUGIN_DEBUG === 'true') {
+      console.log(``)
+      console.log(process.env)
+      console.log(``)
+    }
+
+    if (!process.env.PLUGIN_WEBHOOK) {
+      console.log(`MN_WEBHOOK ENV variable not set!`);
+      return;
+    }
+
+    if (!process.env.PLUGIN_CHANNEL) {
+      console.log(`MN_CHANNEL ENV variable not set!`);
+      return;
+    }
+
+    if (!process.env.PLUGIN_MODE) {
+      process.env.PLUGIN_MODE = 'NOTIFICATION';
+    }
+
+    let _title = process.env.PLUGIN_TITLE || `New release of ${process.env.DRONE_REPO}@${process.env.DRONE_TAG}!`;
+    let _titleLink = process.env.PLUGIN_TITLE_LINK || ``;
+    let _text = process.env.PLUGIN_TEXT || ``;
+    let _mailToSubject = process.env.PLUGIN_MAILTO_SUBJECT || _title;
+    let _mailToLabel = process.env.PLUGIN_MAILTO_LABEL || `Send notification email`;
+    let _mailtoCC = process.env.PLUGIN_MAILTO_CC || ``;
+
+    _text = _text.replace(/\\n/gm, `\n`);
+
+    for (const key in process.env) {
+      if (process.env.hasOwnProperty(key)) {
+        _title = _title.replace(`{{${key}}}`, process.env[key]);
+        _titleLink = _titleLink.replace(`{{${key}}}`, process.env[key]);
+        _text = _text.replace(`{{${key}}}`, process.env[key]);
+        _mailToSubject = _mailToSubject.replace(`{{${key}}}`, process.env[key]);
+        _mailToLabel = _mailToLabel.replace(`{{${key}}}`, process.env[key]);
+        _mailtoCC = _mailtoCC.replace(`{{${key}}}`, process.env[key]);
+      }
+    }
+
+    const body = { 
+      channel: process.env.PLUGIN_CHANNEL,
+      username: process.env.PLUGIN_USERNAME || 'MatterNotify',
+      icon_url: process.env.PLUGIN_ICON_URL || 'https://i.postimg.cc/G2J204Ls/notification-bell.png',
+    }
+
+    switch (process.env.PLUGIN_MODE) {
+      case 'NOTIFICATION':
+        _text = MatterNotify.getMailToMarkdown(_mailToLabel, _mailToSubject, _mailtoCC) + _text;
+        body.attachments = [
+          {
+            color: process.env.PLUGIN_COLOR || "#1bba56",
+            title: _title,
+            title_link: _titleLink,
+            text: _text,
+          },
+        ];
+        break;
+      default:
+        body.text = _text;
+        break;
+    }
+
+    const resp = await fetch(
+      process.env.PLUGIN_WEBHOOK, 
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      }
+    );
+
+    console.log(`Mattermost Response: ${resp.statusText}`);
+  }
+
+  static getMailToMarkdown(_mailtoLabel, _mailtoSubject, _mailtoCC) {
+    let _text = ``;
+
+    if (process.env.PLUGIN_MAILTO && emailRegex.test(process.env.PLUGIN_MAILTO)) {
+      let _ccAddrs = [];
+
+      if (_mailtoCC) {
+        for (const _cc of _mailtoCC.split(',')) {
+          if (emailRegex.test(_cc) && _cc !== process.env.PLUGIN_MAILTO) {
+            _ccAddrs.push(_cc);
+          }
+        }
+      }
+
+      _text = `:email: [${_mailtoLabel}](mailto:${process.env.PLUGIN_MAILTO}?subject=${encodeURIComponent(_mailtoSubject)}&cc=${_ccAddrs.join(',')})\n\n`;
+    } else {
+      if (process.env.PLUGIN_DEBUG === 'true') {
+        console.log(`MN_MAILTO ENV variable not set or invalid!`);
+      }
+    }
+
+    return _text;
+  }
+}
+
+try {
+  MatterNotify.execute();
+} catch (error) {
+  console.log(`Error!`, error);
+}
